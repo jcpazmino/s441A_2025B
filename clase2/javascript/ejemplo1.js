462007f413ebfc9a85cfacad2ce92895761f099a
jcpazmino <jcpazmino44@hotmail.comm>
2025-08-15 11:16:37 -0500

    se termina la clase 2

--- clase2/javascript/ejemplo1.js
+++ clase2/javascript/ejemplo1.js
@@ -1,9 +1,21 @@
-function mostrarMensaje() {
-    nombre = document.getElementById("input1").value;
-    
-    if(nombre === "") {
-        alert("Por favor, ingrese un nombre.");
-        return;
+function validarInformacion() {
+    //validar los campos del formulario utilizando un ciclo, posicionarse en el campo vacio, validar el formato del email    
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var inputs = document.querySelectorAll("input[type='text'], input[type='email']");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === "") {
+            alert("Por favor, complete todos los campos.");
+            inputs[i].focus();
+            return;
+        }else if (inputs[i].type === "email") {
+            // Validar formato del email
+            var email = inputs[i].value;
+            if (!emailPattern.test(email)) {
+                alert("Por favor, ingrese un correo electrónico válido.");
+                inputs[i].focus();
+                return;
+            }
+        }
     }
-    alert("¡Hola " + nombre + "! Este es un mensaje de ejemplo.");
+    alert("Formulario enviado correctamente.");
 }
